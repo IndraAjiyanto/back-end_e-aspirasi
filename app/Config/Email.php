7c22fc6297586fe60bffbef827dd0262a8ea56a8
IndraAjiyanto <indraajiyanto052@gmail.com>
2025-04-15 14:16:03 +0700

    login

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'indrajajal32@gmail.com';
+    public string $fromName   = 'e-aspirasi';
     public string $recipients = '';
 
     /**
